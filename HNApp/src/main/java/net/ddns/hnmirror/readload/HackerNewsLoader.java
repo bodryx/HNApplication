1f7f8b1380af609f837fb5e5fd33d1be24b192dc
bodryx <bodryx@gmail.com>
2018-02-09 21:57:40 -0500

    stupid mistake

--- HNApp/src/main/java/net/ddns/hnmirror/readload/HackerNewsLoader.java
+++ HNApp/src/main/java/net/ddns/hnmirror/readload/HackerNewsLoader.java
@@ -31,7 +31,9 @@ public class HackerNewsLoader {
 			DateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 			Date date = new Date();
 			String dateStr = dateFormat.format(date);
-			String fileName = "new_stories_" + dateStr + ".log";    //"C:\\IDES\\HNMirrorLogs\\new_stories_" + dateStr + ".log";
+			String fileName = "new_stories_" + dateStr + ".log"; // "C:\\IDES\\HNMirrorLogs\\new_stories_"
+																	// + dateStr
+																	// + ".log";
 			System.out.println("fileName = " + fileName);
 			File file = new File(fileName);
 
@@ -43,34 +45,50 @@ public class HackerNewsLoader {
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
 			String content = null;
-
+			String titleTmp, authorTmp, textTmp, urlTmp;
 			for (int i = 0; i < newStoriesArray.size(); i++) {
-				// for (int i = newStoriesArray.size() - 1; i >= 0; i--) {
 				JSONObject jsonObject = storiesReader.getStory((long) newStoriesArray.get(i));
 				if (jsonObject != null) {
-					long itemId = (long) jsonObject.get("id");//
-					boolean flag = false;//
-					for (int j = 0; j < itemIdArray.length; j++) {//
-						if (itemId == itemIdArray[j]) {//
-							flag = true;//
-							break;//
-						} //
-					} //
-					if (!flag) {//
-						// if
-						// (!(Factory.getInstance().getStoryDAO().isStoryInDataBase((long)jsonObject.get("id"))))
-						// {
+					long itemId = (long) jsonObject.get("id");
+					boolean flag = false;
+					for (int j = 0; j < itemIdArray.length; j++) {
+						if (itemId == itemIdArray[j]) {
+							flag = true;
+							break;
+						}
+					}
+					if (!flag) {
 						Story st1 = new Story();
 						st1.setItemId((long) jsonObject.get("id"));
-						st1.setTitle((String) jsonObject.get("title"));
-						st1.setAuthor((String) jsonObject.get("by"));
-						st1.setText((String) jsonObject.get("text"));
-						st1.setUrl((String) jsonObject.get("url"));
+
+						titleTmp = (String) jsonObject.get("title");
+						if (titleTmp.length() > 500)
+							st1.setTitle(titleTmp.substring(0, 500)); // 500
+						else
+							st1.setTitle(titleTmp);
+						
+						authorTmp = (String) jsonObject.get("by");
+						if (authorTmp.length() > 30)
+							st1.setAuthor(authorTmp.substring(0, 30)); // 30
+						else
+							st1.setAuthor(authorTmp);
+						
+						textTmp = (String) jsonObject.get("text");
+						if (textTmp.length() > 3000)
+							st1.setText(textTmp.substring(0, 3000)); // 3000
+						else
+							st1.setText(textTmp);
+						
+						urlTmp = (String) jsonObject.get("url");
+						if (urlTmp.length() > 500)
+							st1.setUrl(urlTmp.substring(0, 500)); // 500
+						else
+							st1.setUrl(urlTmp);
+						
 						st1.setType((String) jsonObject.get("type"));
 						st1.setTime((long) jsonObject.get("time"));
 						st1.setScore((long) jsonObject.get("score"));
-						
-						
+
 						Factory.getInstance().getStoryDAO().addStory(st1);
 						System.out.println("i= " + i);
 
@@ -78,7 +96,7 @@ public class HackerNewsLoader {
 						bw.write(content);
 					} else {
 						System.out.println("not new = " + i);
-						break; //////
+						break;
 					}
 				}
 			}
